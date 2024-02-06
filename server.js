df7d11f46874c4cdd30d1e1cc541cbb09d03e9bf
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-02-06 23:10:27 +0900

    feat: develop author router ejs pege

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 console.log("Web serverni boshlash");
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err)
+    } else {
+     user = JSON.parse(data);
+    }
+});
 // 1 kirish kod
 app.use(express.static("public"));
 app.use(express.json());
@@ -18,9 +29,14 @@ console.log(req.body);
 res.json({test: "success" });
 });
 
+app.get('/author', (req, res) => {
+res.render("author", { user: user })
+});
+
 app.get("/", function(req, res) {
     res.render("harid");
 });
+
 
 const server = http.createServer(app);
 let PORT = 2000;
