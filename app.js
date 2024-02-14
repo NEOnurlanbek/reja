0c94f5a8d873a38f7ef6c96eee0c940dc6fcc583
NEOnurlanbek <kushbakovnurlan447@gmail.com>
2024-02-14 17:32:50 +0900

    feat:frontEnd javaScript

--- app.js
+++ app.js
@@ -22,12 +22,8 @@ app.post("/create-item", (req, res) => {
     console.log(req.body);
     const new_reja = req.body.reja;
     db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-        if(err) {
-            console.log(err);
-            res.end("someting went worong");
-        }else {
-            res.end("successffuly added");
-        }
+        console.log(data.ops);
+        res.json(data.ops[0]);
     });
 });
 
